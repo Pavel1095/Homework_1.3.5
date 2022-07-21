4063649e08683bc6bba49e29097cdb35c8c793ca
Pavel1095 <9153220501@mail.ru>
2022-07-21 09:06:55 +0300

    задание 5 выполнено.

---
+++ src/Main.java
@@ -0,0 +1,47 @@
+public class Main {
+    public static void main(String[] args) {
+            //Задание 5
+        int monthOfYearNumber = 1;
+
+        switch (monthOfYearNumber) {
+            case 1:
+                System.out.println("зима");
+                break;
+            case 2:
+                System.out.println("зима");
+                break;
+            case 3:
+                System.out.println("весна");
+                break;
+            case 4:
+                System.out.println("весна");
+                break;
+            case 5:
+                System.out.println("весна");
+                break;
+            case 6:
+                System.out.println("лето");
+                break;
+            case 7:
+                System.out.println("лето");
+                break;
+            case 8:
+                System.out.println("лето");
+                break;
+            case 9:
+                System.out.println("осень");
+                break;
+            case 10:
+                System.out.println("осень");
+                break;
+            case 11:
+                System.out.println("осень");
+                break;
+            case 12:
+                System.out.println("зима");
+                break;
+            default:
+                System.out.println("такого месяца ен существует.");
+        }
+    }
+}
